4fe3945072151b7c55eeb93774421ba628fb822a
GeorgeGRz <ice18390099@uniwa.gr>
2021-11-30 16:14:14 +0200

    changed server

--- task1-statistics/flaskServer/server.py
+++ task1-statistics/flaskServer/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+app = Flask(__name__, static_folder='../reactApp/build')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
