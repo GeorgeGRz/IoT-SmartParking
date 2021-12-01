962ce1a9e3df5e0f182bf0494d40b72f14fff154
MargaKMus <75671329+MargaKMus@users.noreply.github.com>
2021-12-01 16:14:12 +0200

    Add files via upload

--- task1-statistics/reactApp/src/App.js
+++ task1-statistics/reactApp/src/App.js
@@ -1,52 +1,26 @@
-import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Statistics from './components/pages/Statistics';
+import Car from './components/pages/Car';
+import Register from './components/pages/Register';
 
-/*function App() {
+function App() {
   return (
     <>
       <Router>
         <Navbar />
-          
-            <Route path='/' exact  />
-          
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/statistics' component={Statistics} />
+          <Route path='/car' component={Car} />
+          <Route path='/register' component={Register} />
+        </Routes>
       </Router>
     </>
   );
-}*/
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-//export default App;
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-export default function App() {
-  return <Doughnut data={data} />;
 }
+
+export default App;
